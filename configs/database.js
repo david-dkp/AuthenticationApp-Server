f63848a47f883ea214e42e1134031395a8f65c5a
david-dkp <david.dekeuwer@gmail.com>
2021-12-28 18:10:31 +0100

    feat: add new models inits in database config

--- configs/database.js
+++ configs/database.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require("sequelize")
 const User = require("../models/User")
 const FederatedCredential = require("../models/FederatedCredential")
+const Room = require("../models/Room");
+const Member = require("../models/Member");
 
 const initialize = async () => {
     const sequelize = new Sequelize({dialect: "sqlite", storage: "databases/database.sqlite"})
@@ -9,6 +11,8 @@ const initialize = async () => {
     //Init models
     User.initModel(sequelize)
     FederatedCredential.initModel(sequelize)
+    Room.initModel(sequelize)
+    Member.initModel(sequelize)
 
     await sequelize.sync()
 }
