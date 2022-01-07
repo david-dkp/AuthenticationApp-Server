4b7e371755360be3ddaf27dfa6fb83d52371fc07
david-dkp <david.dekeuwer@gmail.com>
2022-01-07 17:13:24 +0100

    fix: now properly set JWT in cookie, and redirect in the home page

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,16 +1,33 @@
 const checkNotAuth = require("../middlewares/checkNotAuth");
 const passport = require("passport");
 const Router = require("express").Router
-
+const jwt = require("jsonwebtoken")
 const router = Router()
 
-router.use(checkNotAuth({authRedirect: "/"}))
+router.use(checkNotAuth({}))
 
-router.post("/", checkNotAuth({authRedirect: "/"}), passport.authenticate("local"), (req, res) => {
-    res.status(200).redirect("/")
-})
+const createAndSendJwt = (req, res) => {
+    jwt.sign(
+        {userId: req.user.id},
+        "secret",
+        {algorithm: 'HS256'},
+        (err, token) => {
+            if (err) {
+                console.log(err)
+                return res.status(500).json({
+                    type: "Error",
+                    message: "Something went wrong, please try again"
+                })
+            }
 
-router.get("/google", passport.authenticate("google", {scope: ["email", "profile"]}))
+            return res.status(200).cookie("jwt", token).redirect("/")
+        }
+    )
+}
+
+router.post("/", passport.authenticate("local", {session: false}), createAndSendJwt)
+
+router.get("/google", passport.authenticate("google", {session: false, scope: ["email", "profile"]}))
 
 
 module.exports = router
--- routes/oauth2Router.js
+++ routes/oauth2Router.js
@@ -1,13 +1,30 @@
 const passport = require("passport")
 const checkNotAuth = require("../middlewares/checkNotAuth");
+const jwt = require("jsonwebtoken");
 const Router = require("express").Router
 const router = Router()
 
+const createAndSendJwt = (req, res) => {
+        jwt.sign(
+            {userId: req.user.id},
+            "secret",
+            {algorithm: 'HS256'},
+            (err, token) => {
+                    if (err) {
+                            console.log(err)
+                            return res.status(500).json({
+                                    type: "Error",
+                                    message: "Something went wrong, please try again"
+                            })
+                    }
+
+                    return res.status(200).cookie("jwt", token).redirect("http://localhost:3000/")
+            }
+        )
+}
 
 router.use(checkNotAuth({authRedirect: "/"}))
 
-router.get("/redirect/google", passport.authenticate("google"), (req, res) => {
-        res.status(200).redirect("http://localhost:3000/")
-})
+router.get("/redirect/google", passport.authenticate("google"), createAndSendJwt)
 
 module.exports = router
